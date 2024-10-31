d62dd8f9a80a10498ae3c065a983ea7546dce310
Thales R. Menezes <thalesmenezes286@gmail.com>
2024-10-30 21:40:16 -0300

    feat: created repositories

---
+++ eventostec/src/main/java/com/dev/eventostec/repositories/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.dev.eventostec.repositories;
+
+import com.dev.eventostec.domain.coupon.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+}
