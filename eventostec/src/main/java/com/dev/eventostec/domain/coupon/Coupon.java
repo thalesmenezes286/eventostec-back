4fa41994e8d03a8d400ac99978cafa70989a49a7
Thales R. Menezes <thalesmenezes286@gmail.com>
2024-10-30 21:32:35 -0300

    feat: created entitys

---
+++ eventostec/src/main/java/com/dev/eventostec/domain/coupon/Coupon.java
@@ -0,0 +1,30 @@
+package com.dev.eventostec.domain.coupon;
+
+import com.dev.eventostec.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "coupon")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
